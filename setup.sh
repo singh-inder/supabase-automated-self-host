80b9591d81559f57714a1fafe09bd5ef47e4078d
Inder Singh <85822513+singh-inder@users.noreply.github.com>
2026-08-15 14:46:37 +0530

    fix

--- setup.sh
+++ setup.sh
@@ -431,7 +431,7 @@ else
 	update_env_vars "PROXY_AUTH_USERNAME=$username" "PROXY_AUTH_PASSWORD='$password'"
 fi
 
-if [[ "$proxy" == "nginx" && "$CI" = true ]]; then
+if [[ "$proxy" == "nginx" && "$CI" == true ]]; then
 	# https://github.com/JonasAlfredsson/docker-nginx-certbot/blob/master/docs/advanced_usage.md#local-ca
 	proxy_service_yaml="${proxy_service_yaml} | .services.nginx.environment.USE_LOCAL_CA=1"
 fi
